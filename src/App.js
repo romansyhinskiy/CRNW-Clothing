754724a296cba6bfa21248d5348489ee1ed3f272
Roman Syhinskiy <romansyhinskiy@gmail.com>
2021-01-19 18:33:55 +0200

    add storing authentificated users into firesore and sign up component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import './App.css';
 import Header from './components/header/header.component.jsx';
@@ -19,13 +19,31 @@ class App extends React.Component {
     }
   }
 
+  
+
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth  => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot( snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+          console.log(this.state);
+        });
+
+      }
+      
+        this.setState({currentUser: userAuth})
+      
+      // createUserProfileDocument(user);
+    });
   }
 
   componentWillUnmount(){
