81d34480eb5ce03fe6dc8262831611bd3d175100
Roman Syhinskiy <romansyhinskiy@gmail.com>
2021-02-08 18:53:02 +0200

    add directory reducer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store, persistor} from "./redux/store";
+import {PersistGate} from 'redux-persist/integration/react'
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <Router>
+            <PersistGate persistor={persistor}>
+                <App/>
+            </PersistGate>
+        </Router>
+    </Provider>,
+    document.getElementById("root")
 );
